bd6568280574de17c3c4adf7f8b6117a03b87304
Nicolas Paul <n@nc0.fr>
2023-10-01 21:41:28 +0200

    Remove helper functions

--- pkg/config/starlark.go
+++ pkg/config/starlark.go
@@ -35,12 +35,7 @@ package config
 
 import (
 	"fmt"
-	"go.nc0.fr/svgu/pkg/config/lib/bzr"
-	"go.nc0.fr/svgu/pkg/config/lib/fossil"
-	"go.nc0.fr/svgu/pkg/config/lib/git"
-	"go.nc0.fr/svgu/pkg/config/lib/hg"
 	"go.nc0.fr/svgu/pkg/config/lib/prelude"
-	"go.nc0.fr/svgu/pkg/config/lib/svn"
 	"go.nc0.fr/svgu/pkg/types"
 	"go.starlark.net/starlark"
 )
@@ -52,10 +47,8 @@ import (
 func ExecConfig(fl string) (*types.Index, error) {
 	th := &starlark.Thread{
 		Name: "exec " + fl,
-		Load: load,
 	}
 
-	// TODO(nc0): add built-ins
 	env := starlark.StringDict{
 		"index":  starlark.NewBuiltin("index", prelude.InternIndex),
 		"module": starlark.NewBuiltin("module", prelude.InternModule),
@@ -70,22 +63,4 @@ func ExecConfig(fl string) (*types.Index, error) {
 	}
 
 	return &prelude.Registered, nil
-}
-
-// load loads a module from the given path.
-func load(t *starlark.Thread, module string) (starlark.StringDict, error) {
-	switch module {
-	case "@svgu/git.star": // git
-		return git.LoadGitModule(t)
-	case "@svgu/hg.star": // mercurial
-		return hg.LoadHgModule(t)
-	case "@svgu/svn.star": // subversion
-		return svn.LoadSvnModule(t)
-	case "@svgu/fossil.star": // fossil
-		return fossil.LoadFossilModule(t)
-	case "@svgu/bzr.star": // bazaar
-		return bzr.LoadBzrModule(t)
-	default:
-		return nil, fmt.Errorf("unknown module %q", module)
-	}
 }
